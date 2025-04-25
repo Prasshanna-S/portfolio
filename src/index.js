2a23c4cd8887e0c6bc87668cbbca65bd3ddfc298
prasshanna <prasshanna1@gmail.com>
2025-04-24 22:38:30 -0400

    no marquee

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import {Clock} from "./clock";
-import { Marquee } from "./marquee";
+// import { Marquee } from "./marquee";
 
 import {reveal} from "./helpers/reveal";
 
 const clock = new Clock(".hero_clock");
 
-const marqueeElements = document.querySelectorAll("textmarquee");
-marqueeElements.forEach((marqueeElement) => new Marquee(marqueeElement));
+// const marqueeElements = document.querySelectorAll("textmarquee");
+// marqueeElements.forEach((marqueeElement) => new Marquee(marqueeElement));
 reveal();
