cefa0ad76f7ece17698ebd2a9f868eb1bcb18129
prasshanna <prasshanna1@gmail.com>
2025-04-27 21:51:38 -0400

    added grid import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { Clock } from "./clock.js";
- import { Marquee } from "./marquee.js";
+import { Clock } from "./clock";
+import { Marquee } from "./marquee";
+import { Grid } from "./grid";
 
-import { reveal } from "./helpers/reveal.js";
+import { reveal } from "./helpers/reveal";
 
 const clock = new Clock(".hero_clock");
 
- const marqueeElements = document.querySelectorAll(".textmarquee");
+const marqueeElements = document.querySelectorAll(".marquee");
 marqueeElements.forEach(
   (marqueeElement, index) => new Marquee(marqueeElement, index + 1 * 20)
 );
-
-const grid = new grid();
+const grid = new Grid();
 reveal();
